26da46df56e1ae183f803dda1c591c77bcf1569e
Ahmed Rangel <ahmedrangel@outlook.com>
2023-07-12 18:40:36 -0500

    Optimized scraper with new method, updated fetch url, cheerio and jsonpath packages no longer needed, carousel media checker implemented in a better way
    Optimized scraper with new method, updated fetch url, cheerio and jsonpath packages no longer needed, carousel media checker implemented in a better way

--- scraper.js
+++ scraper.js
@@ -1,10 +1,7 @@
-import * as cheerio from "cheerio";
-import jp from "jsonpath";
-
 // Instgram post or reel URL
 const url = "https://www.instagram.com/reel/CtjoC2BNsB2/?utm_source=ig_web_copy_link&igshid=MzRlODBiNWFlZA==" // url example
 const _cookie = `fbm_124024...dab7cd8"`;    // required! get your Cookie from your browser
-const _userAgent = "Mozilla/5.0.../537.36"; // required! get your User-Agent from your browser
+const _userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"; // or get your User-Agent from your browser
 const _xIgAppId = "93661974...";            // required! get your X-Ig-App-Id from your browser
 
 // Function to get instagram post ID from any url string
@@ -20,11 +17,11 @@ const getId = (url) => {
 
 const idUrl = getId(url);
 
-// Scraper
+// Fetch data from instagram post
 if (!idUrl) {
   console.log('Invalid URL');
 } else {
-  const response = await fetch(`https://www.instagram.com/p/${idUrl}/`, {
+  const response = await fetch(`https://www.instagram.com/p/${idUrl}?__a=1&__d=di`, {
     headers: {
       "cookie": _cookie,
       "user-agent": _userAgent,
@@ -32,20 +29,22 @@ if (!idUrl) {
       ["sec-fetch-site"]: "same-origin"
     }
   });
+  const json = await response.json();
+  const items = json.items[0];
+  let carousel_media = [];
 
-  const html = await response.text();
-  const body = cheerio.load(html);
-  const scripts = [];
-  body("script").each((i,el) => {
-      const script = body(el).html();
-      if (script.includes(`"items"`)) {
-          scripts.push(script);
-      }
-  });
-
-  const json = JSON.parse(scripts).require;
-  const items = jp.query(json, "$..[?(@.items)].items[0]")[0];
-  const carousel_media = [];
+  // Check if post is a carousel post
+  items.product_type === "carousel_container" ? (() => {
+    items.carousel_media.forEach(element => {
+      carousel_media.push({
+        image_versions: element.image_versions2.candidates,
+        video_versions: element.video_versions
+      })
+    })
+    return carousel_media;
+  })() : carousel_media = null;
+  
+  // Create json data
   const json_data = {
     code: items.code,
     created_at: items.taken_at,
@@ -64,15 +63,7 @@ if (!idUrl) {
     width: items.original_width,
     image_versions: items.image_versions2 !== null ? items.image_versions2.candidates : null,
     video_versions: items.video_versions,
-    carousel_media: items.product_type === "carousel_container" ? (() => {
-      items.carousel_media.forEach(element => {
-        carousel_media.push({
-          image_versions: element.image_versions2.candidates,
-          video_versions: element.video_versions
-        })
-      })
-      return carousel_media;
-    })() : null
+    carousel_media: carousel_media
   }
 
   // Print json data
